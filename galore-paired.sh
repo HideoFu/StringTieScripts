1b0a53e449df55141f17aab5715b4ea289761575
HideoFu <h.fushimi.x689@gmail.com>
2017-07-11 19:05:34 +0900

    Update galore-paired.sh

--- galore-paired.sh
+++ galore-paired.sh
@@ -3,16 +3,17 @@
 # Input files: *_1.fastq, *_2.fastq
 # Output files: *_1_val_1.fq, *_2_val_2.fq
 
-CORE=$EXPcore    # number of CPU cores. exported from 'main.sh' and so on.
+CORE=$EXPcore    # number of CPU cores
 
-cd ..
+cd ..  # ~/
 mkdir Raw
 mkdir Trimmed
 mkdir FastQC
 
+# trimmed files are in ~/Trimmed/, FastQC results are in ~/FastQC
 trim_galore -o Trimmed --paired ${EXP1}_1.fastq ${EXP1}_2.fastq --fastqc --fastqc_args "--nogroup -t $CORE -o FastQC"
 
-mv ${EXP1}_1.fastq ./Raw
-mv ${EXP1}_2.fastq ./Raw
+mv ${EXP1}_1.fastq ./Raw  # ~/Raw
+mv ${EXP1}_2.fastq ./Raw  # ~/Raw
 
-cd ./HISAT
+cd ./StringTie  # ~/StringTie
